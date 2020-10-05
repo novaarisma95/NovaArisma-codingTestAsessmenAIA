956c3cdc441cfe7f58b3796d2fa38a5534906661
Nova Arisma <novaarisma95@gmail.com>
2020-10-05 22:35:49 +0700

    Initial Commit

---
+++ AIATest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AIATest
+//
+//  Created by Nova Arisma on 10/5/20.
+//  Copyright © 2020 Nova Arisma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
